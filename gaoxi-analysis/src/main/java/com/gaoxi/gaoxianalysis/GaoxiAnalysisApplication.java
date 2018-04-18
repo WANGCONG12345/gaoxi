c2741afbbd71dad718454598c8767f8792e8a6cf
wangcong <Administrator@MS-20160809TEGJ>
2018-04-18 09:43:12 +0800

    高喜项目的创建

---
+++ gaoxi-analysis/src/main/java/com/gaoxi/gaoxianalysis/GaoxiAnalysisApplication.java
@@ -0,0 +1,14 @@
+package com.gaoxi.gaoxianalysis;
+
+import com.gaoxi.gaoxicommonservicefacade.TestCommon;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaoxiAnalysisApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(GaoxiAnalysisApplication.class, args);
+        TestCommon.doSomeThing();
+	}
+}
